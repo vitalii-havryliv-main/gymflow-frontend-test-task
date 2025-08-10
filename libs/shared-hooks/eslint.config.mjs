a6c6c0fd9262e56b65ec45049466bd678dbd0f6f
Vitalii Havryliv <havryliv.vitalii.work@gmail.com>
2025-08-11 00:16:18 +0300

    chore(shared-hooks): extend root eslint config and add json parser to fix TS parsing issues

--- libs/shared-hooks/eslint.config.mjs
+++ libs/shared-hooks/eslint.config.mjs
@@ -1,10 +1,22 @@
-import nx from '@nx/eslint-plugin';
+import baseConfig from '../../eslint.config.mjs';
 
 export default [
-  ...nx.configs['flat/base'],
+  ...baseConfig,
   {
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {},
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+            '{projectRoot}/vite.config.{js,ts,mjs,mts}',
+          ],
+        },
+      ],
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
   },
 ];
-
